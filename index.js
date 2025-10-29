6d808106277b48e4d36fc297e2e4b2d4ec35aff6
BrysonTran <beastmodebryson789@gmail.com>
2025-10-29 13:30:45 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World from Node and Render!');
+  res.send('Hello World!');
 });
 
 app.listen(port, '0.0.0.0', () => {
